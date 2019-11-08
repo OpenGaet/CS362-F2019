8d735d9276a98947d995220958d9ca5d070aac2f
Gaetan Ingrassia <ingrassg@flip2.engr.oregonstate.edu>
2019-11-08 00:10:07 -0800

    unit test 2

--- projects/ingrassg/dominion/unittest2.c
+++ projects/ingrassg/dominion/unittest2.c
@@ -9,14 +9,14 @@
 
 void confirm (int passed) {
 	if (passed == 1)
-		printf("PASSED\n");
+		printf("PASSED: ");
 	else
-		printf("FAILED\n");
+		printf("FAILED: ");
 }
 
 
 int main() {
-    	int choice1 = 0;
+    	int choice1, choice2, handPos;
       	int seed = 1000;
     	int numPlayers = 2;
 	int thisPlayer = 0;
@@ -24,16 +24,17 @@ int main() {
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
 			sea_hag, tribute, smithy, council_room};
 
+	
+	printf("----------------- Testing Minion: ----------------\n");
+
+	// ----------- TEST 1: Choice 1 (+2 Coin) --------------
+	printf("\n-- TEST 1: Choice 1 --\n");
+
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
 
-	printf("----------------- Testing Baron: ----------------\n");
-
-	// ----------- TEST 1: Player Has Estate Card --------------
-	printf("\n-- TEST 1: Player Has Estate Card --\n");
-
 	// Set player hand
-	G.hand[thisPlayer][0] = baron;
+	G.hand[thisPlayer][0] = minion;
 	G.hand[thisPlayer][1] = copper;
 	G.hand[thisPlayer][2] = duchy;
 	G.hand[thisPlayer][3] = estate;
@@ -42,63 +43,56 @@ int main() {
 	// copy the game state to a test case
 	memcpy(&testG, &G, sizeof(struct gameState));
 	choice1 = 1;
-	effectBaron(choice1, &testG, thisPlayer);
+	choice2 = 0;
+	handPos = 0;
+	effectMinion(choice1, choice2, &testG, handPos, thisPlayer);	
 
-	G.coins += 4;//Add 4 coins to the amount of coins
+	G.coins += 2;//Add 4 coins to the amount of coins
     	G.handCount[thisPlayer]--;
 
-	printf("coins = %d, expected = %d\n", testG.coins, G.coins);
 	confirm(testG.coins == G.coins);
-	printf("Hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer]);
+	printf("coins = %d, expected = %d\n", testG.coins, G.coins);
 	confirm(testG.handCount[thisPlayer] == G.handCount[thisPlayer]);
-	
+	printf("Hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer]);
+		
 
-	// ----------- TEST 2: Player Does Not Have Estate Card but There are None to Get --------------
-	printf("\n-- TEST 2: Player Does Not Have Estate Card --\n");
+	// ----------- TEST 2: Choice 2 (discard hand, +4 cards, other players do the same if they have >4 cards --------------
+	printf("\n-- TEST 2: Choice 2 --\n");
 
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
 
 	// Set player hand
-	G.hand[thisPlayer][0] = baron;
+	G.hand[thisPlayer][0] = minion;
 	G.hand[thisPlayer][1] = copper;
 	G.hand[thisPlayer][2] = duchy;
 	G.hand[thisPlayer][3] = smithy;
 	G.hand[thisPlayer][4] = feast;
+
+	// Set opponent hand
+	G.handCount[thisPlayer + 1] = 5;
+	G.hand[thisPlayer + 1][0] = minion;
+	G.hand[thisPlayer + 1][1] = copper;
+	G.hand[thisPlayer + 1][2] = duchy;
+	G.hand[thisPlayer + 1][3] = smithy;
+	G.hand[thisPlayer + 1][4] = feast;
 
 	// Copy the game state to a test case
-	G.supplyCount[estate] = 0;
-	G.supplyCount[duchy] = 0;
-	G.supplyCount[copper] = 0;
 	memcpy(&testG, &G, sizeof(struct gameState));
 	choice1 = 0;
-	effectBaron(choice1, &testG, thisPlayer);
+	choice2 = 1;
+	handPos = 0;
+	effectMinion(choice1, choice2, &testG, handPos, thisPlayer);	
 	discardCard(0, thisPlayer, &G, 0);
+	G.handCount[thisPlayer + 1] = 4;
 
-	printf("coins = %d, expected = %d\n", testG.coins, G.coins);
 	confirm(testG.coins == G.coins);
-	printf("Hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer]);
+	printf("Coins = %d, Expected = %d\n", testG.coins, G.coins);
 	confirm(testG.handCount[thisPlayer] == G.handCount[thisPlayer]);
-	printf("Is Game Over = %d, expected = %d\n", isGameOver(&testG), isGameOver(&G));
-	confirm(isGameOver(&testG) == isGameOver(&G));
-
-	// ----------- TEST 3: No Estate Card but Tries to Discard One --------------
-
-	printf("\n-- TEST 3: Player Does Not Have an Estate Card but Tries to Discard One --\n");
-
-	// initialize a game state and player cards
-	initializeGame(numPlayers, k, seed, &G);
-
-	G.hand[thisPlayer][0] = baron;
-	G.hand[thisPlayer][1] = copper;
-	G.hand[thisPlayer][2] = duchy;
-	G.hand[thisPlayer][3] = smithy;
-	G.hand[thisPlayer][4] = feast;
-
-	// copy the game state to a test case
-	memcpy(&testG, &G, sizeof(struct gameState));
-	choice1 = 1;
-	effectBaron(choice1, &testG, thisPlayer);
+	printf("Hand Count = %d, Expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer]);
+	confirm(testG.handCount[thisPlayer] == G.handCount[thisPlayer]);
+	printf("Opponent Hand Count = %d, Expected = %d\n", testG.handCount[thisPlayer + 1], G.handCount[thisPlayer + 1]);
+	
 	
 	printf("\n >>>>> Testing complete <<<<<\n\n");
 
